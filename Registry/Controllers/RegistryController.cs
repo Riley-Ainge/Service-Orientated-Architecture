2128652ee93a101c961530df2762fd3dce000523
Riley-Ainge <117593905+Riley-Ainge@users.noreply.github.com>
2022-11-07 15:13:14 +0800

    Add files via upload

---
+++ Registry/Controllers/RegistryController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataTypes;
+using Registry.Models;
+using System.ServiceModel;
+using Authenticator;
+
+namespace Registry.Controllers
+{
+    [RoutePrefix("api/registry")]
+    public class RegistryController : ApiController
+    {
+        [Route("publish/{token}")]
+        [Route("publish")]
+        [HttpPut]
+        public IHttpActionResult Publish(int token, ServiceDescription service)
+        {
+            if (validateToken(token) != "validated")
+            {
+                return Ok(new Result("Denied", "Authentication Error"));
+            }
+            RegistryDataTier.addService(service);
+            return Ok(new Result("Successful", ""));
+        }
+        [Route("search/{term}/{token}")]
+        [Route("search")]
+        [HttpPost]
+        public IHttpActionResult Search(string term, int token)
+        {
+            if (validateToken(token) != "validated")
+            {
+                return Ok(new Result("Denied", "Authentication Error"));
+            }
+            List<ServiceDescription> services = RegistryDataTier.getServices();
+            List<ServiceDescription> foundServices = new List<ServiceDescription>();
+            foreach(ServiceDescription item in services)
+            {
+                if(item.description.Contains(term))
+                {
+                    foundServices.Add(item);
+                }
+            }
+            return Ok(foundServices);
+        }
+        [Route("getAll/{token}")]
+        [Route("getAll")]
+        [HttpGet]
+        public IHttpActionResult allServices(int token)
+        {
+            if (validateToken(token) != "validated")
+            {
+                return Ok(new Result("Denied", "Authentication Error"));
+            }
+            return Ok(RegistryDataTier.getServices());
+        }
+        [Route("unpublish/{endpoint}/{token}")]
+        [Route("unpublish")]
+        [HttpDelete]
+        public IHttpActionResult unPublish(string endpoint, int token)
+        {
+            if (validateToken(token) != "validated")
+            {
+                return Ok(new Result("Denied", "Authentication Error"));
+            }
+            List<ServiceDescription> services = RegistryDataTier.getServices();
+            for(int i = 0; i < services.Count; i++)
+            {
+                if (services[i].endpoint.Contains(endpoint))
+                {
+                    RegistryDataTier.removeAt(i);
+                    return Ok(new Result("Successful", "Removed"));
+                }
+            }
+            return Ok(new Result("Unsuccessful", "Endpoint not found"));
+        }
+        /*[Route("setPath/{e}")]
+        [Route("setPath")]
+        [HttpPut]
+        public void setPath(string path)
+        {
+            RegistryDataTier.setFilePath(path + "services.txt");
+        }*/
+        private string validateToken(int token)
+        {
+            ChannelFactory<AuthenticationServiceInterface> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8200/AuthenticationService";
+            foobFactory = new ChannelFactory<AuthenticationServiceInterface>(tcp, URL);
+            AuthenticationServiceInterface foob = foobFactory.CreateChannel();
+            //foob.setPath("C:\\Users\\gameb\\OneDrive\\Documents\\Assignment1\\");
+            return foob.validate(token);
+        }
+    }
+}
---
+++ Registry/Models/RegistryDataTier.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using DataTypes;
+using System.Text;
+using Newtonsoft.Json;
+
+
+namespace Registry.Models
+{
+    public static class RegistryDataTier
+    {
+        //public static String path = "C:\\Users\\gameb\\OneDrive\\Documents\\Assignment1\\registry.txt";
+        public static List<ServiceDescription> getServices()
+        {
+            var result = new List<ServiceDescription>();
+            FileStream fs = File.Open(GlobalPath.path+ "registry.txt", FileMode.Open);
+            using (var reader = new StreamReader(fs))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        result.Add(JsonConvert.DeserializeObject<ServiceDescription>(line));
+                    }
+                }
+            }
+            fs.Close();
+            return result;
+            /*string json = File.ReadAllText(path);
+            List<ServiceDescription> services = JsonConvert.DeserializeObject<List<ServiceDescription>>(json);
+            return services;*/
+        }
+        public static ServiceDescription getService(int i)
+        {
+            string json = File.ReadAllText(GlobalPath.path+"registry.txt");
+            List<ServiceDescription> services = JsonConvert.DeserializeObject<List<ServiceDescription>>(json);
+            return services[i];
+        }
+        public async static void addService(ServiceDescription service)
+        {
+            /*if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write, FileShare.None))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(service));
+                fs.Write(info, 0, info.Length);
+            }*/
+            StreamWriter file = new StreamWriter(GlobalPath.path+ "registry.txt", append: true);
+            await file.WriteLineAsync(JsonConvert.SerializeObject(service));
+            file.Close();
+        }
+        /*public static void setFilePath(String _path)
+        {
+            path = _path + "registry.txt";
+        }*/
+        public async static void removeAt(int i)
+        {
+            List<ServiceDescription> services = getServices();
+            services.RemoveAt(i);
+            File.WriteAllText(GlobalPath.path+ "registry.txt", "");
+            StreamWriter file = new StreamWriter(GlobalPath.path+ "registry.txt", append: true);
+            foreach (ServiceDescription service in services)
+            {
+                await file.WriteLineAsync(JsonConvert.SerializeObject(service));
+            }
+            file.Close();
+            //File.WriteAllText(path, JsonConvert.SerializeObject(services));
+        }
+    }
+}
